8549e7946a7e02ae38ad268991df196fb6f1e76d
ярик <85576863+randomniht@users.noreply.github.com>
2025-06-16 21:49:23 +0500

    16/06

---
+++ py/55/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template_string, render_template
+
+app = Flask(__name__)
+
+buy = ['cheese','watermelon','cherry']
+
+todo = [
+    {'id': 1,'name':'Погулять','status':'не сделано'},
+    {'id': 2,'name': 'Поспать', 'status': 'сделано'}
+]
+
+country = [
+    {'id': 1,'country_name':'Китай','capital':'Пекин','population':'21 843 000'},
+    {'id': 2,'country_name': 'ДРК', 'capital': 'Киншаса', 'population': '14 740 000'},
+    {'id': 3,'country_name': 'Япония', 'capital': 'Токио', 'population': '14 094 034'},
+    {'id': 4,'country_name': 'Россия', 'capital': 'Москва', 'population': '13 149 803'},
+    {'id': 5,'country_name': 'Индонезия', 'capital': 'Джакарта', 'population': '11 249 000'}
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', buy=buy, data=todo)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/country')
+def country_pages():
+    return render_template('country.html', country=todo)
+
+
+@app.route('/country/<int:country_index>')
+def single_country_pages(country_index):
+    single_contry = todo[country_index]
+    return render_template('contry_detail.html', single_contry = single_contry)
+
+
+app.run(debug=True, port=5007)
